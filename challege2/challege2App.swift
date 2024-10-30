f390065a0baadcf2f83711fcb15361469b1db955
rashaq3390 <rashaq3390@gmail.com>
2024-10-30 13:58:42 +0300

    Initial Commit

---
+++ challege2/challege2App.swift
@@ -0,0 +1,17 @@
+//
+//  challege2App.swift
+//  challege2
+//
+//  Created by رشا القرني on 27/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct challege2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
